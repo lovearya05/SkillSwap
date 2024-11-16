0abcd1da93065a3be5462045beb1d71918f8f21f
Indrashekar <indrashekar.bg@boom.tv>
2024-11-16 13:52:24 +0530

    added Icons code

--- src/Navigation/NavigationStack.js
+++ src/Navigation/NavigationStack.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
@@ -11,10 +12,52 @@ const Tab = createBottomTabNavigator();
 
 const BottomTabsNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={{
-      headerShown: false,
-    }} options={{headerShown: false}} >
-      <Tab.Screen name="HomeStack" component={HomeStackNavigator} />
+    // <Tab.Navigator screenOptions={{
+    //   headerShown: false,
+    // }} options={{headerShown: false}} >
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false, // Hides the text labels below icons
+        tabBarIcon: ({ focused, size }) => {
+          let iconSource;
+
+          // Set the appropriate icon for each tab
+          if (route.name === 'Home') {
+            iconSource = focused
+              ? require('../assets/ImageIcons/Blue_Home_Icon.png') // Active icon
+              : require('../assets/ImageIcons/Home_Icon.png'); // Inactive icon
+          } else if (route.name === 'Connect') {
+            iconSource = focused
+              ? require('../assets/ImageIcons/Blue_Network_Icon.png')
+              : require('../assets/ImageIcons/Network_Icon.png');
+          } else if (route.name === 'Profile') {
+            iconSource = focused
+              ? require('../assets/ImageIcons/Blue_Profile_Icon.png')
+              : require('../assets/ImageIcons/Profile_Icon.png');
+          }
+
+          // Return the Image component for the icon
+          return (
+            <Image
+              source={iconSource}
+              style={{
+                width: size,
+                height: size,
+                resizeMode: 'contain',
+              }}
+            />
+          );
+        },
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          paddingBottom: 5,
+          height: 60, // Adjust height as needed
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeStackNavigator} />
       <Tab.Screen name="Connect" component={ConnectionsStackNavigator} />
       <Tab.Screen name="Profile" component={ProfileStackNavigator} />
     </Tab.Navigator>
